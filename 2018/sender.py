7a6cc6803eae9dea53e7abbfa65269e07c3f9402
Joseph Bentivegna <jbentivegna15@gmail.com>
2018-05-11 21:20:17 -0400

    updated sender

--- 2018/sender.py
+++ 2018/sender.py
@@ -2,10 +2,10 @@
 
 import logging
 import socket
-
 import channelsimulator
 import utils
 import sys
+import zlib
 
 class Sender(object):
 
@@ -14,31 +14,101 @@ class Sender(object):
 
         self.inbound_port = inbound_port
         self.outbound_port = outbound_port
-        self.simulator = channelsimulator.ChannelSimulator(inbound_port=inbound_port, outbound_port=outbound_port,
-                                                           debug_level=debug_level)
+        self.simulator = channelsimulator.ChannelSimulator(inbound_port=inbound_port, outbound_port=outbound_port, debug_level=debug_level)
         self.simulator.sndr_setup(timeout)
         self.simulator.rcvr_setup(timeout)
-
-    def send(self, data):
-        raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
-
 
 class BogoSender(Sender):
 
     def __init__(self):
-        super(BogoSender, self).__init__()
+        super(BogoSender, self).__init__(timeout=.01)
+        
+    def int2bi(self, number):
+        
+        #converts the sequence number to a 32bit/4byte binary value
+        biNum = bin(number)[2:].zfill(32)
+        
+        #converts the binary value to a 4 length bytearray
+        bytArr = bytearray([(a & 0xFF000000) >> 24, (a & 0xFF0000) >> 16, (a & 0xFF00) >> 8, (a & 0xFF)])
+        return bytArr
+
+    def bi2int(self, bytArr):
+        
+        biNum = ''.join([string.zfill(s,8) for s in map(lambda n : n[2:], map(bin,bytArr))])
+        return int(biNum)
 
     def send(self, data):
-        self.logger.info("Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
+        pDataSize = 1016 # + 4 bytes for seq number + 4 bytes for checksum
+        pList = []
+        windSize = 10
+        
+        # create list of packets
+        for i in xrange(0, len(data), pDataSize):
+            upper = i + pDataSize
+            
+            if upper > len(data):
+                upper = len(data)
+            
+            # create components
+            seqNum = bytearray([0x00, 0x00, 0x00, 0x00])            # dan will fix
+            datachunk = data[i:upper]
+            chksum = int2bi(sum(datachunk))
+            
+            # combine components
+            packet = seqNum + chksum + datachunk
+            
+            # add packet to packet list
+            plist.append({
+                "packet": bytearray(packet),
+                "ack": False,
+                "sent": False
+            })
+            
+        lower = 0
+        upper = 0
+        
+        # check window size at end of data
+        numPackets = len(plist)
+        if numPackets < windSize:
+            windSize = numPackets
+
         while True:
             try:
-                self.simulator.u_send(data)  # send data
-                ack = self.simulator.u_receive()  # receive ACK
-                self.logger.info("Got ACK from socket: {}".format(
-                    ack.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
-                break
+                    upper = lower + windSize
+                    
+                    if upper > numPackets:
+                        upper = numPackets
+                        
+                    for i in xrange(lower, upper):
+                        if not plist[i]['sent']:
+                            self.simulator.u_send(plist[i]['packet'])
+                            plist[i]['sent'] = True
+                            
+                    while True:
+                        
+                        ack = self.simulator.u_receive()
+                        chksum = sum(ack[:4])
+                        ack_chksum = bi2int(ack[-4:])
+                        
+                        if chksum == ack_chksum:
+                            
+                            seqNum = ack[:4]
+                            plist[seqNum]['ack'] = True
+                            
+                            if seqNum == plist[upper - 1]['packet'][0]:
+                                break
+                                
+                    for i in xrange(lower, upper):
+                        if not plist[i]['ack']:
+                            break                             
+                        else:
+                            lower += windSize
+                
             except socket.timeout:
-                pass
+                
+                for i in xrange(lower, upper):
+                    if not plist[i]['ack']:
+                        self.simulator.u_send(plist[i]['packet'])
 
 
 if __name__ == "__main__":
