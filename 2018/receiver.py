0771b95108b0d5fb8e2c43984826c321c3b19b9e
Daniel Park <danpark0605@gmail.com>
2018-05-11 21:56:29 -0400

    updated receiver.py

--- 2018/receiver.py
+++ 2018/receiver.py
@@ -1,7 +1,7 @@
 # Written by S. Mevawala, modified by D. Gitzel
 
 import logging
-
+import string
 import channelsimulator
 import utils
 import sys
@@ -9,7 +9,7 @@ import socket
 
 class Receiver(object):
 
-    def __init__(self, inbound_port=50005, outbound_port=50006, timeout=10, debug_level=logging.INFO):
+    def __init__(self, inbound_port=50005, outbound_port=50006, timeout=0.01, debug_level=logging.INFO):
         self.logger = utils.Logger(self.__class__.__name__, debug_level)
 
         self.inbound_port = inbound_port
@@ -24,22 +24,78 @@ class Receiver(object):
 
 
 class BogoReceiver(Receiver):
+    PACKET_SIZE = 1016
+    WINDOW =
     ACK_DATA = bytes(123)
 
     def __init__(self):
         super(BogoReceiver, self).__init__()
 
+    def checksum(data):
+        sum1 = 0
+        sum2 = 0
+        i = 0
+        while i < len(data):
+            sum1 = (sum1 + ord(data[i]))%65535
+            sum2 = (sum2 + sum1)%65535
+            i+=1
+        x = sum1*65536 + sum2
+        checksum = bytearray.fromhex('{:08x}'.format(x))
+        pad = bytearray(32-len(checksum))
+        return pad + checksum
+
+    def int2bi(self, number):
+        #converts the sequence number to a 32bit/4byte binary value
+        biNum = bin(number)[2:].zfill(32)
+        #converts the binary value to a 4 length bytearray
+        bytArr = bytearray([(a & 0xFF000000) >> 24, (a & 0xFF0000) >> 16, (a & 0xFF00) >> 8, (a & 0xFF)])
+        return bytArr
+
+    def bi2int(self, bytArr):
+        biNum = ''.join([string.zfill(s,8) for s in map(lambda n : n[2:], map(bin, bytArr))])
+        return int(biNum)
+
     def receive(self):
+        end = False
         self.logger.info("Receiving on port: {} and replying with ACK on port: {}".format(self.inbound_port, self.outbound_port))
+        rList = dict()
+
         while True:
             try:
-                 data = self.simulator.u_receive()  # receive data
-                 self.logger.info("Got data from socket: {}".format(
-                     data.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
-	         sys.stdout.write(data)
-                 self.simulator.u_send(BogoReceiver.ACK_DATA)  # send ACK
+                packet = self.simulator.u_receive()  # receive data
+                #if the packet size is smaller than 1024, then set end = True
+                #else:
+                self.logger.info("Got data from socket: {}".format(
+                    data.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
+
+                #extract seqNum checksum and calculate data's checksum
+                seqNum = bi2int(packet[:3])
+                chkSum = bi2int(packet[-3:])
+                dataSum = INSERT THIS LATER
+
+                #if the data checksum is equal to calculated checksum, add to list of received packets and create an ACK packet
+                if dataSum == chkSum:
+                    rList[seqNum] = {
+                        'packet': bytearray(packet),
+                        'acked': False,
+                    }
+                    #create a checksum for the sequence number of the ACK corresponding to the received packet being sent back
+                    ack = bytearray(seqNum + sum(seqNum))
+                    #send ACK packet
+                    self.simulator.u_send(ack)
+                    rList[seqNum]['acked'] = True
+                else:
+                    pass
+
             except socket.timeout:
-                sys.exit()
+                if end:
+                    break
+                else:
+                    pass
+
+        for entry in rList:
+            if entry != None:
+                sys.stdout.write(entry['packet'][4:-4])
 
 if __name__ == "__main__":
     # test out BogoReceiver
---
+++ 2018/send.py
@@ -0,0 +1,10 @@
+import sender
+import socket
+import sys
+import time
+import string
+
+class jankSender(sender.BogoSender):
+
+    def __init__(self):
+        super(jankSender, self).__int__()
